758e10c568a3eff374bea076385fdf0697e6caeb
Jonathan Allen <jallen@snow.edu>
2023-10-09 12:44:47 -0600

    strip out starter implementation

--- site.js
+++ site.js
@@ -1,4 +1,367 @@
-import { books } from "./books.js";
+const books = {
+  books: [
+    {
+      title: "To Kill a Mockingbird",
+      pages: 281,
+      description:
+        "A classic novel by Harper Lee that explores racial injustice in a small Southern town.",
+      rating: 4,
+      number_sold: 5000000,
+    },
+    {
+      title: "1984",
+      pages: 328,
+      description:
+        "George Orwell's dystopian masterpiece depicting a totalitarian regime and thought control.",
+      rating: 4,
+      number_sold: 4000000,
+    },
+    {
+      title: "The Great Gatsby",
+      pages: 180,
+      description:
+        "F. Scott Fitzgerald's tale of wealth, decadence, and unrequited love in the Roaring Twenties.",
+      rating: 4,
+      number_sold: 3500000,
+    },
+    {
+      title: "Harry Potter and the Sorcerer's Stone",
+      pages: 320,
+      description:
+        "J.K. Rowling's magical adventure follows the young wizard Harry Potter.",
+      rating: 4,
+      number_sold: 7500000,
+    },
+    {
+      title: "The Catcher in the Rye",
+      pages: 224,
+      description:
+        "J.D. Salinger's iconic novel of teenage angst and rebellion in New York City.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "Pride and Prejudice",
+      pages: 279,
+      description:
+        "Jane Austen's timeless romance novel featuring the headstrong Elizabeth Bennet and the enigmatic Mr. Darcy.",
+      rating: 4,
+      number_sold: 3500000,
+    },
+    {
+      title: "The Hobbit",
+      pages: 310,
+      description:
+        "J.R.R. Tolkien's fantasy adventure about the hobbit Bilbo Baggins on a quest for treasure and adventure.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Hunger Games",
+      pages: 374,
+      description:
+        "Suzanne Collins' dystopian series starter where teenagers are forced to fight to the death for entertainment.",
+      rating: 4,
+      number_sold: 4000000,
+    },
+    {
+      title: "Brave New World",
+      pages: 325,
+      description:
+        "Aldous Huxley's vision of a futuristic, controlled society that explores themes of individuality and conformity.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Lord of the Rings",
+      pages: 1178,
+      description:
+        "J.R.R. Tolkien's epic fantasy trilogy, a tale of friendship, adventure, and the battle against evil.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Alchemist",
+      pages: 197,
+      description:
+        "Paulo Coelho's philosophical novel about a shepherd's journey to discover his personal legend.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Da Vinci Code",
+      pages: 454,
+      description:
+        "Dan Brown's gripping mystery thriller involving a Harvard symbologist and a secret society.",
+      rating: 4,
+      number_sold: 6000000,
+    },
+    {
+      title: "The Shining",
+      pages: 447,
+      description:
+        "Stephen King's chilling story of a family's descent into madness at the isolated Overlook Hotel.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Girl with the Dragon Tattoo",
+      pages: 590,
+      description:
+        "Stieg Larsson's gripping mystery featuring investigative journalist Mikael Blomkvist and hacker Lisbeth Salander.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "Fahrenheit 451",
+      pages: 249,
+      description:
+        "Ray Bradbury's dystopian novel depicting a future society where books are banned and burned.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Road",
+      pages: 287,
+      description:
+        "Cormac McCarthy's post-apocalyptic journey of a father and son trying to survive in a desolate world.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "Gone with the Wind",
+      pages: 1037,
+      description:
+        "Margaret Mitchell's epic tale of love and loss set against the backdrop of the American Civil War.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Chronicles of Narnia",
+      pages: 764,
+      description:
+        "C.S. Lewis' beloved series of fantasy novels, filled with magical adventures and talking animals.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Fault in Our Stars",
+      pages: 313,
+      description:
+        "John Green's heartwarming and heartbreaking story of two teenagers with cancer who fall in love.",
+      rating: 4,
+      number_sold: 3500000,
+    },
+    {
+      title: "The Road Less Traveled",
+      pages: 316,
+      description:
+        "M. Scott Peck's self-help classic that explores the path to personal growth and spiritual fulfillment.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Martian",
+      pages: 369,
+      description:
+        "Andy Weir's gripping tale of an astronaut stranded on Mars and his fight for survival.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Help",
+      pages: 464,
+      description:
+        "Kathryn Stockett's novel about African American maids in 1960s Mississippi and their fight for equality.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Road to Character",
+      pages: 320,
+      description:
+        "David Brooks' exploration of moral character and the pursuit of a deeper and more meaningful life.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "The Kite Runner",
+      pages: 371,
+      description:
+        "Khaled Hosseini's emotionally charged story of friendship, betrayal, and redemption in Afghanistan.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Book Thief",
+      pages: 552,
+      description:
+        "Markus Zusak's novel set in Nazi Germany, narrated by Death, and featuring a young girl's love for books.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "A Song of Ice and Fire",
+      pages: 6941,
+      description:
+        "George R.R. Martin's epic fantasy series filled with political intrigue, dragons, and battles for power.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "The Catcher in the Rye",
+      pages: 224,
+      description:
+        "J.D. Salinger's iconic novel of teenage angst and rebellion in New York City.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Goldfinch",
+      pages: 771,
+      description:
+        "Donna Tartt's Pulitzer Prize-winning novel about a young boy who steals a priceless painting.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Night Circus",
+      pages: 391,
+      description:
+        "Erin Morgenstern's enchanting tale of a magical competition between two young illusionists.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Name of the Wind",
+      pages: 662,
+      description:
+        "Patrick Rothfuss' epic fantasy about the gifted young musician and wizard, Kvothe.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "The Help",
+      pages: 464,
+      description:
+        "Kathryn Stockett's novel about African American maids in 1960s Mississippi and their fight for equality.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Road to Character",
+      pages: 320,
+      description:
+        "David Brooks' exploration of moral character and the pursuit of a deeper and more meaningful life.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "The Kite Runner",
+      pages: 371,
+      description:
+        "Khaled Hosseini's emotionally charged story of friendship, betrayal, and redemption in Afghanistan.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Book Thief",
+      pages: 552,
+      description:
+        "Markus Zusak's novel set in Nazi Germany, narrated by Death, and featuring a young girl's love for books.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "A Song of Ice and Fire",
+      pages: 6941,
+      description:
+        "George R.R. Martin's epic fantasy series filled with political intrigue, dragons, and battles for power.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "The Goldfinch",
+      pages: 771,
+      description:
+        "Donna Tartt's Pulitzer Prize-winning novel about a young boy who steals a priceless painting.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Night Circus",
+      pages: 391,
+      description:
+        "Erin Morgenstern's enchanting tale of a magical competition between two young illusionists.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Name of the Wind",
+      pages: 662,
+      description:
+        "Patrick Rothfuss' epic fantasy about the gifted young musician and wizard, Kvothe.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "The Road",
+      pages: 287,
+      description:
+        "Cormac McCarthy's post-apocalyptic journey of a father and son trying to survive in a desolate world.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+    {
+      title: "Gone with the Wind",
+      pages: 1037,
+      description:
+        "Margaret Mitchell's epic tale of love and loss set against the backdrop of the American Civil War.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Chronicles of Narnia",
+      pages: 764,
+      description:
+        "C.S. Lewis' beloved series of fantasy novels, filled with magical adventures and talking animals.",
+      rating: 4,
+      number_sold: 2500000,
+    },
+    {
+      title: "The Fault in Our Stars",
+      pages: 313,
+      description:
+        "John Green's heartwarming and heartbreaking story of two teenagers with cancer who fall in love.",
+      rating: 4,
+      number_sold: 3500000,
+    },
+    {
+      title: "The Road Less Traveled",
+      pages: 316,
+      description:
+        "M. Scott Peck's self-help classic that explores the path to personal growth and spiritual fulfillment.",
+      rating: 4,
+      number_sold: 2000000,
+    },
+    {
+      title: "The Martian",
+      pages: 369,
+      description:
+        "Andy Weir's gripping tale of an astronaut stranded on Mars and his fight for survival.",
+      rating: 4,
+      number_sold: 3000000,
+    },
+    {
+      title: "The Color Purple",
+      pages: 295,
+      description:
+        "Alice Walker's powerful novel about the struggles and triumphs of African American women in the South.",
+      rating: 4,
+      number_sold: 1500000,
+    },
+  ],
+};
 
 const readingList = document.getElementById("reading-list");
 readingList.addEventListener("drop", drop);
@@ -7,32 +370,38 @@ console.log("readingList", readingList);
 
 // Function to display books in the left pane
 function displayBooks() {
-  const bookList = document.getElementById("book-list");
-  books.books.forEach((book, index) => {
-    const bookItem = document.createElement("div");
-    bookItem.className = "book draggable";
-    bookItem.draggable = true;
-    bookItem.dataset.index = index;
-    bookItem.innerHTML = `
-                    <img src=https://picsum.photos/seed/${
-                      index + 1
-                    }/150/150 style="margin-right: 1em;" >
-                    <div>
-                    <h3>${book.title}</h3>
-                    <p>Description: ${book.description}</p>
-                    <p>Rating: ${renderStars(book.rating)}</p>
-                    </div>
-                `;
-    bookItem.ondragstart = drag;
-    bookList.appendChild(bookItem);
-    console.log(index, book.title);
-  });
+    //implement this
 }
+
+const getLeftBookTemplate = (book, index) => {
+  return `
+        <img src=https://picsum.photos/seed/${
+            index + 1
+        }/150/150 style="margin-right: 1em;" >
+        <div>
+            <h3>${book.title}</h3>
+            <p>Description: ${book.description}</p>
+            <p>Rating: ${renderStars(book.rating)}</p>
+        </div>
+    `;
+};
+
+const getRightBookTemplate = (book, index) => `
+    <img src=https://picsum.photos/seed/${
+    parseInt(index) + 1
+    }/300/300 style="; margin-right: 1em;" >
+    <div>
+        <h3>${book.title}</h3>
+        <p>Pages: ${book.pages}</p>
+        <p>Sold: ${book.number_sold}</p>
+        <p>Description: ${book.description}</p>
+        <p>Rating: ${renderStars(book.rating)}</p>
+    </div>
+`;
 
 // Drag and drop functionality
 function drag(event) {
-  console.log("drag()", event);
-  event.dataTransfer.setData("text/plain", event.target.dataset.index);
+  //implement this (don't forget to send the index of the book)
 }
 
 function allowDrop(event) {
@@ -40,26 +409,7 @@ function allowDrop(event) {
 }
 
 function drop(event) {
-  event.preventDefault();
-  const index = event.dataTransfer.getData("text/plain");
-  const book = books.books[index];
-  console.log("drop()", event, index);
-  const readingList = document.getElementById("reading-list");
-  const bookItem = document.createElement("div");
-  bookItem.className = "book";
-  bookItem.innerHTML = `
-                <img src=https://picsum.photos/seed/${
-                  parseInt(index) + 1
-                }/300/300 style="; margin-right: 1em;" >
-                <div>
-                <h3>${book.title}</h3>
-                <p>Pages: ${book.pages}</p>
-                <p>Sold: ${book.number_sold}</p>
-                <p>Description: ${book.description}</p>
-                <p>Rating: ${renderStars(book.rating)}</p>
-                </div>
-            `;
-  readingList.appendChild(bookItem);
+  //implement this
 }
 
 function renderStars(rating) {
